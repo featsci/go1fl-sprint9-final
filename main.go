71180d42e50435c221b2c6f4911bfec72b50fda3
Kost <you@example.com>
2025-06-06 09:23:53 +0000

    Начал отладку функции generateRandomElements

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 const (
@@ -12,29 +13,39 @@ const (
 // generateRandomElements generates random elements.
 func generateRandomElements(size int) []int {
 	// ваш код здесь
+	s := []int{} // make([]int, size)
+
+	for i := range size {
+		_ = i
+		s = append(s, rand.Intn(SIZE))
+	}
+
+	// fmt.Println(s, rand.Intn(SIZE))
+	return s
 }
 
-// maximum returns the maximum number of elements.
-func maximum(data []int) int {
-	// ваш код здесь
-}
+// // maximum returns the maximum number of elements.
+// func maximum(data []int) int {
+// 	// ваш код здесь
+// }
 
-// maxChunks returns the maximum number of elements in a chunks.
-func maxChunks(data []int) int {
-	// ваш код здесь
-}
+// // maxChunks returns the maximum number of elements in a chunks.
+// func maxChunks(data []int) int {
+// 	// ваш код здесь
+// }
 
 func main() {
-	fmt.Printf("Генерируем %d целых чисел", SIZE)
+	fmt.Printf("Генерируем %d целых чисел\n", SIZE)
 	// ваш код здесь
+	generateRandomElements(3)
 
 	fmt.Println("Ищем максимальное значение в один поток")
 	// ваш код здесь
 
-	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
+	// fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 
-	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
-	// ваш код здесь
+	// fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
+	// // ваш код здесь
 
-	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
+	// fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 }
