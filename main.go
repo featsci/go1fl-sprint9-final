77b87766466e216ef2781e04b8c708abff9737a7
Kost <you@example.com>
2025-06-09 11:12:58 +0000

    Переделал функцию generateRandomElements в main.go

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 	"slices"
+	"sync"
 	"time"
 )
 
@@ -20,7 +21,7 @@ func generateRandomElements(size int) []int {
 		return s
 	}
 	for i := range size {
-		s[i] = rand.Intn(SIZE)
+		s[i] = rand.Intn(SIZE * SIZE)
 		// s[i] = 0
 	}
 	return s
@@ -45,29 +46,79 @@ func maxChunks(data []int) int {
 	if len(data) == 0 || len(data) == 1 {
 		return 0
 	}
-	// var div [][]int
 
-	for i := 0; i < len(data); i += CHUNKS {
-		end := i + CHUNKS
+	var wg sync.WaitGroup
+	// mySlice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
+	// numSubSlices := 3
+	subSlices := make([][]int, CHUNKS)
+	sliceLen := len(data)
+	subSliceSize := sliceLen / CHUNKS
+	remainder := sliceLen % CHUNKS
+	start := 0
+	var sL []int
 
-		if end > len(data) {
-			end = len(data)
-		}
-
-		// div = append(div, data[i:end])
-		fmt.Println(slices.Max(data[i:end]))
+	for i := 0; i < CHUNKS; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			end := start + subSliceSize
+			if i < remainder {
+				end++
+			}
+			subSlices[i] = data[start:end]
+			start = end
+			sL = append(sL, maximum(subSlices[i]))
+		}(i)
 	}
+	wg.Wait()
+	// fmt.Println(subSlices) // Output: [[1 2 3 4] [5 6 7] [8 9 10]]
+	return maximum(sL)
 
-	// fmt.Printf("%#v\n", div)
+	/*
+	   // ваш код здесь
+	   // создайте переменную типа sync.WaitGroup и используйте её при запуске и ожидании горутин.
+	   var wg sync.WaitGroup
 
-	return 0
+	   var total []int
+
+	   	if len(data) == 0 || len(data) == 1 {
+	   		return 0
+	   	}
+
+	   // var div [][]int
+
+	   	for i := 0; i < len(data); i += CHUNKS {
+	   		end := i + CHUNKS
+
+	   		if end > len(data) {
+	   			end = len(data)
+	   		}
+	   		wg.Add(1)
+	   		go func(i int) {
+	   			defer wg.Done()
+
+	   			// end := i + CHUNKS
+
+	   			// if end > len(data) {
+	   			// 	end = len(data)
+	   			// }
+
+	   			// div = append(div, data[i:end])
+	   			total = append(total, slices.Max(data[i:end]))
+	   		}(i)
+	   	}
+
+	   // fmt.Printf("%#v\n", div)
+	   wg.Wait()
+	   return slices.Max(total)
+	*/
 }
 
 func main() {
 	fmt.Printf("Генерируем %d целых чисел\n", SIZE)
 	// ваш код здесь
-	g := generateRandomElements(32)
-	fmt.Println(g)
+	g := generateRandomElements(SIZE)
+	// fmt.Println(g)
 
 	fmt.Println("Ищем максимальное значение в один поток")
 	// ваш код здесь
